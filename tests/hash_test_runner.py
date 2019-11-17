ab7431217ce37b55f4a8abcd6e64ee0851b65ca9
Paul Tyshevskyi <ptyshevs@gmail.com>
2019-11-17 12:38:37 +0200

    Add multi-processing to hash test runner, add length unit-tests

--- tests/hash_test_runner.py
+++ tests/hash_test_runner.py
@@ -3,6 +3,7 @@ import subprocess
 import random
 import string
 import sys
+import multiprocessing as mp
 from hash_unit_tests import *
 
 BLACK = '\033[0;30m'
@@ -38,34 +39,24 @@ def gen_rand_string(maxlen=10):
 
 
 def fuzz(ft_ssl_path, subcommand, ssl_binary="openssl", n=100, l=10, verbose=0):
-    command_template = "echo -n '{}' | {} " + subcommand
-    error_log = []
-    for _ in range(n):
-        inp = gen_rand_string(maxlen=l)
+    fuzz_suite = [(f'fuzz{i}', gen_rand_string(maxlen=l)) for i in range(n)]
 
-        my_command = command_template.format(inp, ft_ssl_path)
-        gt_command = command_template.format(inp, ssl_binary)
+    pool_input = [(ft_ssl_path, subcommand, ssl_binary, verbose, test_name, test_input) \
+                  for (test_name, test_input) in fuzz_suite]
 
-        gt_out = run_command(gt_command)
-        my_out = run_command(my_command)
+    errors = pool.starmap(job, pool_input)
+    errors = [e for e in errors if len(e)]
 
-        same_result = gt_out == my_out
-        if not same_result:
-            error_log.append((gt_command, gt_out, my_command, my_out))
-        if verbose == 1 and not same_result:
-            print("[gt] {} != {} [ft]".format(gt_out, my_out))
-        else:
-            print("{}.{}".format(GREEN if same_result else RED, NC), end='', flush=True)
     if not verbose:
-        print("")  # finish line
-    return error_log
+            print("")  # finish line
+    return errors
 
 def output_errors(error_log, file=sys.stdout):
     """
     Error format - tuple of (gt_input, gt_output, my_input, my_output)
     """
     for (gt_in, gt_out, my_in, my_out) in error_log:
-        print("[openssl in]:{}\n[openssl_out]: {}\n[ft_ssl in]:{}\n[ft_ssl out]: {}".format(gt_in, gt_out, my_in, my_out))
+        print("[openssl in]:{}\n[openssl_out]: {}\n[ft_ssl in]:{}\n[ft_ssl out]: {}".format(gt_in, gt_out, my_in, my_out), file=file)
 
 def subcommand_to_unit_tests(subcommand):
     if subcommand == 'sha256':
@@ -73,24 +64,38 @@ def subcommand_to_unit_tests(subcommand):
     else:
         return None
 
-def run_ut_suite(ft_ssl_path, subcommand, test_suite, ssl_binary="openssl", verbose=False):
+def job(ft_ssl_path, subcommand, ssl_binary, verbose, test_name, test_input):
+    command_template = "echo -n '{}' | {} " + subcommand
+
+    my_command = command_template.format(test_input, ft_ssl_path)
+    gt_command = command_template.format(test_input, ssl_binary)
+
+    gt_out = run_command(gt_command)
+    my_out = run_command(my_command)
+
+    same_result = gt_out == my_out
+    if same_result:
+        out = tuple()
+    else:
+        out = (gt_command, gt_out, my_command, my_out)
+
+    if verbose == 1 and not same_result:
+        print("[gt] {} != {} [ft]".format(gt_out, my_out))
+    else:
+        print("{}.{}".format(GREEN if same_result else RED, NC), end='', flush=True)
+
+    return out
+
+def run_ut_suite(ft_ssl_path, subcommand, test_suite, pool, ssl_binary="openssl", verbose=False):
     errors = []
     command_template = "echo -n '{}' | {} " + subcommand
 
-    for (test_name, test_input) in test_suite:
-        my_command = command_template.format(test_input, ft_ssl_path)
-        gt_command = command_template.format(test_input, ssl_binary)
+    pool_input = [(ft_ssl_path, subcommand, ssl_binary, verbose, test_name, test_input) \
+            for (test_name, test_input) in test_suite]
 
-        gt_out = run_command(gt_command)
-        my_out = run_command(my_command)
+    errors = pool.starmap(job, pool_input)
+    errors = [e for e in errors if len(e)]
 
-        same_result = gt_out == my_out
-        if not same_result:
-            errors.append((gt_command, gt_out, my_command, my_out))
-        if verbose == 1 and not same_result:
-            print("[gt] {} != {} [ft]".format(gt_out, my_out))
-        else:
-            print("{}.{}".format(GREEN if same_result else RED, NC), end='', flush=True)
     if not verbose:
         print("")  # finish line
     return errors
@@ -101,7 +106,15 @@ if __name__ == '__main__':
     parser.add_argument("-p", "--path", default="./ft_ssl", help="path to ft_ssl executable")
     parser.add_argument('-f', '--fuzz', default=False, help="Apply fuzzing after unit-tests are run", action='store_true')
     parser.add_argument("-v", '--verbose', default=False, help="Be more verbose", action='store_true')
+    parser.add_argument("-l", '--log', default='error.log', help='File to write diff into')
+    parser.add_argument("-j", "--jobs", default=4, help='Number of workers in a thread pool')
+
     args = parser.parse_args()
+
+    if type(args.jobs) is not int:
+        args.jobs = int(args.jobs)
+
+    pool = mp.Pool(args.jobs)
 
     subcommand = 'sha256'
 
@@ -115,7 +128,7 @@ if __name__ == '__main__':
         print("ERROR: subcommand {} is not recognized".format(subcommand))
         exit(1)
     
-    ut_errors = run_ut_suite(args.path, subcommand, test_suite)
+    ut_errors = run_ut_suite(args.path, subcommand, test_suite, pool, verbose=args.verbose)
     error_log.extend(ut_errors)
     
     # part 2: optional fuzzing
@@ -127,4 +140,6 @@ if __name__ == '__main__':
             error_log.extend(fuzz_errors)
     
     if len(error_log) > 0:
-        output_errors(error_log)
+        with open(args.log, 'w+') as f:
+            output_errors(error_log, f)
+            print("Errors written into", args.log)
--- tests/hash_unit_tests.py
+++ tests/hash_unit_tests.py
@@ -7,8 +7,11 @@ fips180_long_a = ('fips180_long_a', 'a' * 1_000)
 
 fips180_multiblock512 = ('fips180_multiblock512', '"abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu')
 
+def len_check():
+    return [(f'a_len={i}', 'a' * i) for i in range(1, 513)]
+
 def sha256_test_suite():
-    return [empty_input, fips180_simple, fips180_multiblock256, fips180_long_a]
+    return [empty_input, fips180_simple, fips180_multiblock256, fips180_long_a] + len_check()
 
 
 if __name__ == '__main__':
