d0c0d5c5e88d4fd7de41a58b6f076509b700fce1
Paul Tyshevskyi <ptyshevs@gmail.com>
2019-11-17 16:04:56 +0200

    Fixes to test runner (now use subprocess.run and input stdin directly)

--- tests/hash_test_runner.py
+++ tests/hash_test_runner.py
@@ -24,10 +24,10 @@ LCYAN = '\033[1;36m'
 WHITE = '\033[1;37m'
 NC = '\033[0m'
 
-def run_command(command):
+def run_command(command, inp):
     try:
-        out = subprocess.check_output(command, shell=True)
-        return str(out, encoding='utf-8')
+        out = subprocess.run(command, input=inp, encoding='utf-8', shell=True, capture_output=True)
+        return out.stdout
     except subprocess.CalledProcessError as e:
         print(str(e.output, encoding='utf-8'))
         return None
@@ -37,6 +37,43 @@ alphabet = string.ascii_letters + string.digits
 def gen_rand_string(maxlen=10):
     return ''.join(random.choices(alphabet, k=maxlen))
 
+
+
+def output_errors(error_log, file=sys.stdout):
+    """
+    Error format - tuple of (gt_input, gt_output, my_input, my_output)
+    """
+    for (input, gt_in, gt_out, my_in, my_out) in error_log:
+        print("[stdin]:{}\n[openssl in]:{}\n[openssl_out]: {}\n[ft_ssl in]:{}\n[ft_ssl out]: {}\n------------------------------------".format(input, gt_in, gt_out, my_in, my_out), file=file)
+
+def subcommand_to_unit_tests(subcommand):
+    if subcommand == 'sha256':
+        return sha256_test_suite()
+    elif subcommand == 'md5':
+        return md5_test_suite()
+    else:
+        return None
+
+def job(ft_ssl_path, subcommand, ssl_binary, verbose, test_name, test_input):
+    command_template = "{} {}"
+
+    my_command = command_template.format(ft_ssl_path, subcommand)
+    gt_command = command_template.format(ssl_binary, subcommand)
+    gt_out = run_command(gt_command, test_input)
+    my_out = run_command(my_command, test_input)
+
+    same_result = gt_out == my_out
+    if same_result:
+        out = tuple()
+    else:
+        out = (test_input, gt_command, gt_out, my_command, my_out)
+
+    if verbose == 1 and not same_result:
+        print("[gt] {} != {} [ft]".format(gt_out, my_out))
+    else:
+        print("{}.{}".format(GREEN if same_result else RED, NC), end='', flush=True)
+
+    return out
 
 def fuzz(ft_ssl_path, subcommand, ssl_binary="openssl", n=100, l=10, verbose=0):
     fuzz_suite = [(f'fuzz{i}', gen_rand_string(maxlen=l)) for i in range(n)]
@@ -48,45 +85,9 @@ def fuzz(ft_ssl_path, subcommand, ssl_binary="openssl", n=100, l=10, verbose=0):
     errors = [e for e in errors if len(e)]
 
     if not verbose:
-            print("")  # finish line
+        print("")  # finish line
     return errors
 
-def output_errors(error_log, file=sys.stdout):
-    """
-    Error format - tuple of (gt_input, gt_output, my_input, my_output)
-    """
-    for (gt_in, gt_out, my_in, my_out) in error_log:
-        print("[openssl in]:{}\n[openssl_out]: {}\n[ft_ssl in]:{}\n[ft_ssl out]: {}".format(gt_in, gt_out, my_in, my_out), file=file)
-
-def subcommand_to_unit_tests(subcommand):
-    if subcommand == 'sha256':
-        return sha256_test_suite()
-    elif subcommand == 'md5':
-        return md5_test_suite()
-    else:
-        return None
-
-def job(ft_ssl_path, subcommand, ssl_binary, verbose, test_name, test_input):
-    command_template = "echo -n '{}' | {} " + subcommand
-
-    my_command = command_template.format(test_input, ft_ssl_path)
-    gt_command = command_template.format(test_input, ssl_binary)
-
-    gt_out = run_command(gt_command)
-    my_out = run_command(my_command)
-
-    same_result = gt_out == my_out
-    if same_result:
-        out = tuple()
-    else:
-        out = (gt_command, gt_out, my_command, my_out)
-
-    if verbose == 1 and not same_result:
-        print("[gt] {} != {} [ft]".format(gt_out, my_out))
-    else:
-        print("{}.{}".format(GREEN if same_result else RED, NC), end='', flush=True)
-
-    return out
 
 def run_ut_suite(ft_ssl_path, subcommand, test_suite, pool, ssl_binary="openssl", verbose=False):
     errors = []
@@ -104,6 +105,7 @@ def run_ut_suite(ft_ssl_path, subcommand, test_suite, pool, ssl_binary="openssl"
 
 
 if __name__ == '__main__':
+    # print(run_command("openssl md5", 'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq'))
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--subcommand', default='all', help='subcommand to test (most test-suites are shared between all subcommands)')
     parser.add_argument("-p", "--path", default="../ft_ssl", help="path to ft_ssl executable")
@@ -149,7 +151,7 @@ if __name__ == '__main__':
                 print("Fuzzing {} with len={}".format(subcommand, l))
                 fuzz_errors = fuzz(args.path, subcommand, verbose=args.verbose, l=l)
                 error_log.extend(fuzz_errors)
-    
+
     if len(error_log) > 0:
         with open(args.log, 'w+') as f:
             output_errors(error_log, f)
